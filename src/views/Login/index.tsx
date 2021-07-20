b9a5f5e50e28609ef4582f25867838c2113ba7e5
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-07-20 11:23:38 -0300

    feat: userlogin

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,6 +1,8 @@
 import { FormEvent, useCallback, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 import { Input } from "../../components/input";
+import { apiLogin } from "../../services/api";
 import { Container } from "./styles";
 
 interface IDataInputs {
@@ -9,26 +11,42 @@ interface IDataInputs {
 }
 
 export const Login = () => {
-
+  const history = useHistory();
   const [dataInput, setDataInput] = useState<IDataInputs>({} as IDataInputs);
+  console.log(dataInput);
+
+  const handleAuth = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      apiLogin.post("session", dataInput).then((res) => {
+        localStorage.setItem("@token", res.data.token);
+        history.push("/dash");
+      });
+    },
+    [dataInput, history]
+  );
+
   return (
-    <Container>
+    <Container onSubmit={handleAuth}>
       <Input
         name="login"
         placeholder="escreva seu email"
         label="Email"
         type="email"
-        onChange={(e) => setDataInput({ ...dataInput, email: e.target.value })}
+        onChange={(e: any) =>
+          setDataInput({ ...dataInput, email: e.target.value })
+        }
       />
       <Input
         name="password"
         placeholder="escreva sua senha"
         label="password"
         type="password"
-        onChange={(e) =>
+        onChange={(e: any) =>
           setDataInput({ ...dataInput, password: e.target.value })
         }
       />
+      <button type="submit">Entrar</button>
     </Container>
   );
 };
