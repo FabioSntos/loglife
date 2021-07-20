342c9d8a1d3edff54b5c5e7dea16cb9579cb0724
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-07-20 09:31:58 -0300

    chore: react-router-dom

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import { BrowserRoutes, Switch, Route} from 'react-router-dom'
+
+
+
+export const routes = () => {
+  return (
+    <BrowserRoutes>
+      <Route path="/" />
+    </BrowserRoutes>
+  )
+}
