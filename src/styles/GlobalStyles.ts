7d4a0e745682c6dda8054720ac64649d008cd7ce
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-07-20 10:23:31 -0300

    feat: color variables

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,7 +1,12 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyles = createGlobalStyle`
+:root {
+ --green: #33cc95;
+ --blue: #5429cc;
+ --blue-light: #6933ff;
 
+}
 * {
   margin: 0;
   padding: 0;
