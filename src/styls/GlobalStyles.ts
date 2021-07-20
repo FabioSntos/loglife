2c542c3b4d0b27f7b477de3414be88ca31f5052a
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-07-20 09:41:28 -0300

    feat: globalStyles

---
+++ src/styls/GlobalStyles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const GlobalStyles = styled.div`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body,input,textarea,button {
+    font-family:'Poppins', sans-serif;
+    font-weight: 400;
+  }
+`
