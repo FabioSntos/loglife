bee42a033beedacb261caa00b441e57b6347506d
FabioSntos <57735438+FabioSntos@users.noreply.github.com>
2021-07-21 11:28:32 -0300

    feat: page NotFound

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Dashboard } from "../views/dashboard/intex";
 import { Login } from "../views/Login";
+import { NotFound } from "../views/notFound";
 import PrivateRoutes from "./privateRoutes";
 
 export const Router = () => {
@@ -9,6 +10,7 @@ export const Router = () => {
       <Switch>
         <Route exact path="/" component={Login} />
         <PrivateRoutes path="/dashboard" exact component={Dashboard} />
+        <Route path="*" component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/views/notFound/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  height:100vh;
+  
+
+  h1 {
+    font-family: var(--type-second);
+    line-height: 1;
+    font-size: 3rem;
+    margin: 1rem 0;
+    position: relative;
+    z-index: 1;
+  }
+
+`
